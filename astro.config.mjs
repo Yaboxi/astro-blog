d8d7e492a1e7977f4c2bb41468cdaa10799a2ff3
Yaboxi <z.yaboxi@gmail.com>
2023-01-14 07:08:45 +0900

    fix: change serverless to edge settings.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/edge';
 
 // https://astro.build/config
 
