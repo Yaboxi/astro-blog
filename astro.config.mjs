2d622ec46711fd3cc42ed429f1590caee5915f5c
Yaboxi <z.yaboxi@gmail.com>
2023-01-14 07:37:48 +0900

    fix: change serverless settings to static.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
   integrations: [tailwind()]
 });
